3081474b1c00f07fa0f6bd01b05904a8e200e54b
Bunyod Bekpolatov <bekopatov@gmail.com>
2024-03-06 01:55:56 +0500

    lesson_5.homework

---
+++ async.py
@@ -0,0 +1,36 @@
+import asyncio
+import json
+from datetime import datetime
+
+
+async def task2():
+    await asyncio.sleep(3)
+    with open("data__1.json", "r") as file:
+        data = json.load(file)
+        ids = []
+        names = []
+        prices = []
+
+        for i in data["Cars Chevrolet"]:
+            ids.append(i["id"])
+            names.append(i["name"])
+            prices.append(i["price"])
+
+    whole_data = {"ids": ids, "names": names, "prices": prices}
+    await task1(whole_data)
+
+
+async def task1(data):
+    await asyncio.sleep(2)
+    with open("data__2.json", "w") as file1:
+        json.dump(data, file1, indent=4)
+
+
+async def main():
+    await asyncio.gather(task2())
+
+
+if __name__ == "__main__":
+    print(datetime.now().time())
+    asyncio.run(main())
+    print(datetime.now().time())
